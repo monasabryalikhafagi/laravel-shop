f229f0b8e9f4d200d9deb1001a58bcbf11e5e34f
Mona Sabry <mona.sabry@appssquare.com>
2023-05-04 09:27:47 +0300

    Use haruncpi/laravel-user-activity and  laravel/telescope packages

--- Modules/Users/Entities/User.php
+++ Modules/Users/Entities/User.php
@@ -5,9 +5,10 @@ namespace Modules\Users\Entities;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\User as BaseModel;
 use Illuminate\Notifications\Notifiable;
+use Haruncpi\LaravelUserActivity\Traits\Loggable;
 class User extends BaseModel 
 {
-use Notifiable;
+use Notifiable,Loggable;
     
     public function receivesBroadcastNotificationsOn()
     {
